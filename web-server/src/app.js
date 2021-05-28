1696c7f0b3fb235a573b88f2fe181ac80dbe8b20
nparsley <nickparsley5@gmail.com>
2021-05-27 19:04:25 -0700

    start config

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+//heroku config
+const port = process.env.PORT || 3000
 
 // define paths for express config
 const publicDirectory = path.join(__dirname, '../public')
@@ -109,9 +111,10 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
+
 
 
 
