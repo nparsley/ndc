4e70a2f8ed6283f6f648f2465196f046e5b290db
nparsley <nickparsley5@gmail.com>
2021-05-28 21:03:07 -0700

    task-manager setup + mongodb

---
+++ task-manager/mongodb.js
@@ -0,0 +1,22 @@
+// crud ops
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('unable to connect to database')
+    }
+    
+    const db = client.db(databaseName)
+
+
+    db.collection('users').insertOne({
+        name: 'nick',
+        age: 32
+    })
+})
