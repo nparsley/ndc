57db2a28164ba33c25e972c0e9f3ea74b7928287
nparsley <nickparsley5@gmail.com>
2021-05-30 19:51:11 -0700

    mongoose setup

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+
+const me = new User({
+    name: 'nick',
+    age: 'hello world'
+})
+
+
+me.save().then((me) => {
+    console.log(me)
+}).catch((error) => {
+    console.log('error', error)
+})
+
+
+
+
+
+
+
+
+
+
