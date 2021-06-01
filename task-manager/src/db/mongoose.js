bbabd227fe66495728d81ae091f3689c9a513288
nparsley <nickparsley5@gmail.com>
2021-05-31 19:19:25 -0700

    validator add + data validation,sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -9,17 +10,49 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('age must be a positive number')
+            } 
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('password cannot contain password')
+            }
+        }
+        
     }
 })
 
 
-const me = new User({
-    name: 'nick',
-    age: 'hello world'
+/* const me = new User({
+    name: 'mike',
+    email: 'mike@myemail.com',
+    password: 'Password'
 })
 
 
@@ -27,14 +60,32 @@ me.save().then((me) => {
     console.log(me)
 }).catch((error) => {
     console.log('error', error)
+}) */
+
+
+
+const Tasks = mongoose.model('Tasks', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
+const start = new Tasks({
+    description: '    customize tasks    ',
+    
+})
 
-
-
-
-
-
+start.save().then((start) => {
+    console.log(start)
+}).catch((error) => {
+    console.log('error', error)
+})
 
 
 
