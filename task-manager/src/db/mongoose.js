80173a99bd2a2b652bec3943f640e704969f0749
nparsley <nickparsley5@gmail.com>
2021-06-01 17:52:42 -0700

    task creation enpoint

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-// const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,19 +6,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
+//keep file to single responsibility
 
 
-const Tasks = mongoose.model('Tasks', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
 
 
 
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Tasks = require('./models/task')
+
 
 const app = express()
 const port = process.env.port || 3000
@@ -14,10 +16,23 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
         // res.send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    // console.log(req.body)
+    // res.send('testing')
+
+    const task = new Tasks(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
     })
 })
 
