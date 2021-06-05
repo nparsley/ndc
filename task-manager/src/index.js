243e8e1e700c14f12d873c96f97aaeabc5983f03
nparsley <nickparsley5@gmail.com>
2021-06-04 21:33:50 -0700

    deleting endpoints-users,tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -89,7 +89,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+//DELETE - user by id
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
 
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 
 
@@ -164,7 +177,20 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+//DELETE - task by id
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Tasks.findByIdAndDelete(req.params.id)
 
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
 
 
 app.listen(port, () => {
