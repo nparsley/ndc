307bc7a841f15e4a64e111d2612f4d004beb2d3b
nparsley <nickparsley5@gmail.com>
2021-06-24 16:52:20 -0700

    user tests upload + patch

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -105,5 +105,55 @@ test('Should not delete account for user', async () => {
  
 
 
+test('Should upload avatar image', async () => {
+    await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200)
+
+    const user = await User.findById(userOneId)
+    // expect({}).toBe({})
+    expect(user.avatar).toEqual(expect.any(Buffer))
+
+})
+
+
+test('Should update valid user fields', async () => {
+    await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+        name: 'rickrick'
+    })
+    .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('rickrick')
+})
+
+
+test('Should not update invalid user fields', async () => {
+    await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+        location: 'arizona'
+    })
+    .expect(400)
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
